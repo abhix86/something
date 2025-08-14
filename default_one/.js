6458dd5191350ffc6b087431a6f07caef2beed73
abhix86 <uchihamadarat99@gmail.com>
2025-08-14 14:05:31 +0530

    added

---
+++ default_one/.js
@@ -0,0 +1,43 @@
+const words = [" Developer", " Learner", " Reader", " Watcher", " Listener", " Dreamer", " Writer", " Wanderer"];
+const value = document.querySelector(".rand-word");
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function changeWord() {
+  let displayText = ''
+  const randomIndex = Math.floor(Math.random() * words.length);
+  for (let i = 0; i < words[randomIndex].length; i++) {
+    displayText += words[randomIndex][i]
+    value.textContent = displayText
+    await wait(500)
+  }
+
+}
+setInterval(changeWord, 6000)
+
+
+document.querySelectorAll('[data-tooltip]').forEach(el => {
+  el.addEventListener('mouseenter', e => {
+    const tooltip = document.createElement('div');
+    tooltip.className = 'custom-tooltip';
+    tooltip.innerText = el.getAttribute('data-tooltip');
+    document.body.appendChild(tooltip);
+
+    const rect = el.getBoundingClientRect();
+    tooltip.style.left = `${rect.left + rect.width / 2}px`;
+    tooltip.style.top = `${rect.top - 30}px`;
+
+    el._tooltip = tooltip;
+  });
+
+  el.addEventListener('mouseleave', e => {
+    if (el._tooltip) {
+      el._tooltip.remove();
+      el._tooltip = null;
+    }
+  });
+});
+
+
